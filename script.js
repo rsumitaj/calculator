fd9f824cb1d6a666c2065834526657acc5f52d64
rsumitaj <sumitrajtech@gmail.com>
2021-06-26 00:51:41 +0530

    add function for different operators -- final commit

--- script.js
+++ script.js
@@ -9,11 +9,20 @@ for(var i=0;i<buttons.length;i++){
 
     var value = this.getAttribute('data-value');
 
-    if(value=='+' || value=="-" || value=="*" || value=="/"){ // whether an operator is clicked here
+    if(value=='ac'){
+      display.innerHTML="";
+    }else if(value=='sign'){
+      operand1=parseFloat(display.textContent);
+      ans=-1*(operand1);
+      display.innerHTML = ans;
+    }else if(value=='%'){
+      operand1=parseFloat(display.textContent);
+      ans=0.01*(operand1);
+      display.innerHTML=ans;
+    }else if(value=='+' || value=="-" || value=="*" || value=="/"){ // whether an operator is clicked here
       operator=value;
       operand1=parseFloat(display.textContent);
       display.innerHTML="";
-
     }else if(value=='='){
       operand2=parseFloat(display.textContent);
       display.innerHTML="";
@@ -21,6 +30,13 @@ for(var i=0;i<buttons.length;i++){
       var ans = eval(operand1 + operator + operand2);
       // show result 
       display.innerHTML += ans;
+
+      // after first result
+      if(ans){
+        operand1=ans;
+        operand2=null;
+        operator=null;
+      }
 
     }else{
       display.innerText += value;
